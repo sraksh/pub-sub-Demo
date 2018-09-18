f970918e0089d52485be13137b3e0fc97b321ce9
Sumit Kumar Rakshit <sumitkumarrakshit@gmail.com>
2018-09-19 02:20:07 +0530

    corrected initialisations

--- js/pubSubUtil.js
+++ js/pubSubUtil.js
@@ -4,18 +4,18 @@ var pubSubUtil = (function() {
 	var subscribers = {};
 
 	function subscribe(msg, callback) {
-		subscribers[msg] = subscribers[msg] || [];
-		subscribers[msg].push(callback);
+		// subscribers[msg] = subscribers[msg] || [];
+		subscribers[msg] = callback;
 	}
 
 	function publish(msg) {
-		// console.log(arguments);
+		console.log(arguments);
 		var args = Array.prototype.slice.call(arguments);
-		// console.log(args.slice(1));
+		console.log(args.slice(1));
 		if(subscribers[msg]){
-			for(var i=0, n=subscribers[msg].length; i < n; i++) {
-				subscribers[msg][i].apply(null, args.slice(1));
-			}
+			//for(var i=0, n=subscribers[msg].length; i < n; i++) {
+				subscribers[msg].apply(null, args.slice(1));
+			//}
 		}	
 	}
 	return {
