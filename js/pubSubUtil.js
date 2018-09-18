4eb0ae651db220f25c908f232fe2ca9d092fd309
Sumit Kumar Rakshit <sumitkumarrakshit@gmail.com>
2018-09-19 02:22:20 +0530

    removed consoles

--- js/pubSubUtil.js
+++ js/pubSubUtil.js
@@ -4,18 +4,13 @@ var pubSubUtil = (function() {
 	var subscribers = {};
 
 	function subscribe(msg, callback) {
-		// subscribers[msg] = subscribers[msg] || [];
 		subscribers[msg] = callback;
 	}
 
 	function publish(msg) {
-		console.log(arguments);
 		var args = Array.prototype.slice.call(arguments);
-		console.log(args.slice(1));
 		if(subscribers[msg]){
-			//for(var i=0, n=subscribers[msg].length; i < n; i++) {
-				subscribers[msg].apply(null, args.slice(1));
-			//}
+			subscribers[msg].apply(null, args.slice(1));
 		}	
 	}
 	return {
